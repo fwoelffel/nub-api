6d7b26fb6f5510ad75a7e011cd1117519a0d2de6
Frédéric Woelffel <woelffel.f@gmail.com>
2017-09-17 13:12:18 +0200

    feat(user): Add the base API for users

---
+++ api/models/User.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * User.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  schema: true,
+  attributes: {
+    email: {
+      type: 'string',
+      isEmail: true,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: 'string'
+    },
+    username: {
+      type: 'string',
+      required: true
+    },
+    toJSON: () => {
+      const obj = this.toOject();
+      delete obj.password;
+      return obj;
+    }
+  },
+  beforeCreate: async (values, next) => {
+    try {
+      values = await CipherService.hashPassword(values);
+    }
+    catch (err) {
+      return next(err);
+    }
+    return next();
+  },
+  beforeUpdate: async (values, next) => {
+    try {
+      values = await CipherService.hashPassword(values);
+    }
+    catch (err) {
+      return next(err);
+    }
+    return next();
+  }
+
+};
+
