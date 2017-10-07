803f2440147df51244474f002c23466e45fe5b25
Frédéric Woelffel <woelffel.f@gmail.com>
2017-10-08 00:17:18 +0200

    doc(api): Add apidoc

--- api/controllers/AuthenticationController.js
+++ api/controllers/AuthenticationController.js
@@ -36,7 +36,17 @@ function _onPassportAuth(req, res, error, user, info) {
 module.exports = {
 
   /**
-   * `AuthenticationController.signup()`
+   *  @api {post} /auth/signup Sign up a new user.
+   *  @apiName SignUp
+   *  @apiGroup Authentication
+   *
+   *  @apiParam {String} email The user email address.
+   *  @apiParam {String} username The user username.
+   *  @apiParam {String} password The user password.
+   *
+   *  @apiSuccess (201) {Object} data The response payload.
+   *  @apiSuccess (201) {Object} data.user The created user object.
+   *  @apiSuccess (201) {String} data.token The created user JWT.
    */
   signup: async (req, res) => {
     try {
@@ -57,7 +67,16 @@ module.exports = {
   },
 
   /**
-   * `AuthenticationController.signin()`
+   * @api {post} /auth/signin Sign in an existing user.
+   * @apiName SignIn
+   * @apiGroup Authentication
+   *
+   * @apiParam {String} email The user's email.
+   * @apiParam {String} password The user's password.
+   *
+   *  @apiSuccess (200) {Object} data The response payload.
+   *  @apiSuccess (200) {Object} data.user The signed in user object.
+   *  @apiSuccess (200) {String} data.token The signed in user JWT.
    */
   signin: function (req, res) {
     passport.authenticate(
--- api/controllers/SnippetController.js
+++ api/controllers/SnippetController.js
@@ -9,6 +9,20 @@
 
 module.exports = {
 
+  /**
+   * @api {get} /snippets/:id Get the snippet matching the given id.
+   * @apiName GetSnippet
+   * @apiGroup Snippets
+   *
+   * @apiParam {String} id A snippet short id.
+   *
+   * @apiSuccess (200) {Object} data The response payload object.
+   * @apiSuccess (200) {String} data.name The snippet name.
+   * @apiSuccess (200) {String} data.description The snippet description.
+   * @apiSuccess (200) {String} data.content The snippet content.
+   * @apiSuccess (200) {String} data.id The snippet id.
+   * @apiSuccess (200) {String} [data.owner] The snippet owner id.
+   */
   getByShortId: async (req, res) => {
     try {
       const snippet = await Snippet.findOne({
