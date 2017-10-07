ac353047e0a59e7ef9ae896771bbe5300df16f01
Frédéric Woelffel <woelffel.f@gmail.com>
2017-10-08 00:19:09 +0200

    refactor: Remove dead code

--- api/controllers/AuthenticationController.js
+++ api/controllers/AuthenticationController.js
@@ -3,30 +3,6 @@
 const passport = require('passport');
 
 /**
- * Triggers when user authenticates via passport
- * @param {Object} req Request object
- * @param {Object} res Response object
- * @param {Object} error Error object
- * @param {Object} user User profile
- * @param {Object} info Info if some error occurs
- * @private
- */
-function _onPassportAuth(req, res, error, user, info) {
-  if (error) {
-    return res.status(500).json({error: error});
-  }
-  if (!user) {
-    return res.status(401).json({error: info});
-  }
-  else {
-    return res.ok({
-      token: JWTokenService.createToken(user),
-      user: user
-    });
-  }
-}
-
-/**
  * AuthenticationController
  *
  * @description :: Server-side actions for handling incoming requests.
