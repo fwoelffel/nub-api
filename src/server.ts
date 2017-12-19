0f75451d1cfabdf18e58f03dab760e42c5f5be29
Frédéric Woelffel <woelffel.f@gmail.com>
2017-12-19 19:47:26 +0100

    feat(server): Setup a global-scoped validation pipe

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app/app.module';
+import { ValidationPipe } from "@nestjs/common";
 
 const bootstrap = async () => {
 
   const app = await NestFactory.create(ApplicationModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
   console.log('Application is listening on port 3000')
 
