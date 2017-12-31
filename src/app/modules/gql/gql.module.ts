1fbc13b971328f25fd2fd9d0f4cd056a7ac9d95c
Frédéric Woelffel <woelffel.f@gmail.com>
2017-12-31 12:33:33 +0100

    feat(graphql): Add GraphiQL IDE

--- src/app/modules/gql/gql.module.ts
+++ src/app/modules/gql/gql.module.ts
@@ -1,5 +1,5 @@
 import {Module, NestModule, MiddlewaresConsumer, RequestMethod} from '@nestjs/common';
-import {graphqlExpress} from 'apollo-server-express';
+import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
 import {GraphQLFactory, GraphQLModule} from '@nestjs/graphql';
 import {SnippetResolvers} from "./snippet/snippet.resolvers";
 import {SnippetModule} from "../snippet/snippet.module";
@@ -21,6 +21,8 @@ export class GQLModule implements NestModule {
     const typeDefs = this.graphQLFactory.mergeTypesByPaths('./**/*.graphql');
     const schema = this.graphQLFactory.createSchema({ typeDefs });
     consumer
+      .apply(graphiqlExpress({ endpointURL: '/graphql' }))
+      .forRoutes({ path: '/graphiql', method: RequestMethod.GET })
       .apply(graphqlExpress(req => ({ schema, rootValue: req })))
       .forRoutes({ path: '/graphql', method: RequestMethod.ALL });
   }
