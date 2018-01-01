ac6c2cb77e4aad5d6f040f107e0f9f5890bb7f94
Frédéric Woelffel <woelffel.f@gmail.com>
2018-01-01 16:27:00 +0100

    feat(server): Add CORS

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app/app.module';
 import { ValidationPipe } from "@nestjs/common";
+import * as cors from 'cors';
 
 const listeningPort = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 const bootstrap = async () => {
 
   const app = await NestFactory.create(ApplicationModule);
+  app.use(cors());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(listeningPort);
   console.log(`Application is listening on port ${listeningPort}`);
