10881317f8fa70f38102ea7b61c95b6729b5e424
Frédéric Woelffel <woelffel.f@gmail.com>
2018-01-02 22:45:58 +0100

    feat(shared): Create a shared classToPlain interceptor

---
+++ src/app/shared/interceptors/classToPlain.interceptor.ts
@@ -0,0 +1,11 @@
+import { Interceptor, NestInterceptor, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import {classToPlain} from "class-transformer";
+
+@Interceptor()
+export class ClassToPlainInterceptor implements NestInterceptor {
+  intercept(dataOrRequest, context: ExecutionContext, stream$: Observable<any>): Observable<any> {
+    return stream$.map((data) => (classToPlain(data)));
+  }
+}
